6c6556bb56b8254f15d8a9fe7ff8b1616d33d8e7
linxs0211 <ci_lynx@users.noreply.github.com>
2025-03-10 23:46:47 +0800

    [Optimize][Android] use displayManager to monitor refreshRate
     WindowManger.getDefaultDisplay().getRefreshRate() involves IPC, which
     causes performance issues.
    
     Use DisplayManager to monitor the refreshRate instead.
    
     issue:m-6089584522
    
     AutoSubmit:true
    
     AutoLand:release/3.1
    
    Use displayManager to get refreshRate

--- platform/android/lynx_android/src/main/java/com/lynx/tasm/core/VSyncMonitor.java
+++ platform/android/lynx_android/src/main/java/com/lynx/tasm/core/VSyncMonitor.java
@@ -4,8 +4,14 @@
 
 package com.lynx.tasm.core;
 
+import android.hardware.display.DisplayManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.Choreographer;
+import android.view.Display;
 import android.view.WindowManager;
+import androidx.annotation.RequiresApi;
 import com.lynx.tasm.base.CalledByNative;
 import com.lynx.tasm.base.LLog;
 import com.lynx.tasm.utils.CallStackUtil;
@@ -15,9 +21,66 @@ import java.lang.ref.WeakReference;
 public class VSyncMonitor {
   public final static long DEFAULT_FRAME_TIME_NS = 1000000000 / 60;
   private static WeakReference<WindowManager> mWindowManager;
+
+  private static DisplayManager mDisplayManager;
   private static Choreographer sUIThreadChoreographer = null;
+
+  private static boolean mUseDisplayManager = false;
+
+  private static DisplayManager.DisplayListener mDisplayListener = null;
+
+  // FIXME(linxs): shall we need to make it volatile? Actually, we will make the VSyncMonitor to be
+  // called on UI thread soon
+  private static long mFrameRefreshTimeNS = -1;
+
+  @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+  static class DisplayListener implements DisplayManager.DisplayListener {
+    @Override
+    public void onDisplayAdded(int displayId) {}
+
+    @Override
+    public void onDisplayRemoved(int displayId) {}
+
+    @Override
+    public void onDisplayChanged(int displayId) {
+      if (displayId == Display.DEFAULT_DISPLAY && mDisplayManager != null) {
+        updateFreshRate();
+      }
+    }
+  }
+
+  private static void updateFreshRate() {
+    try {
+      final Display primaryDisplay;
+      if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+        primaryDisplay = mDisplayManager.getDisplay(Display.DEFAULT_DISPLAY);
+        float fps = primaryDisplay.getRefreshRate();
+        mFrameRefreshTimeNS = (long) (1000000000 / fps);
+      } else {
+        mFrameRefreshTimeNS = DEFAULT_FRAME_TIME_NS;
+      }
+    } catch (RuntimeException e) {
+      LLog.e("VSyncMonitor",
+          "onDisplayChanged failed: " + CallStackUtil.getStackTraceStringTrimmed(e));
+    }
+  }
+
   public static void setCurrentWindowManager(WindowManager vm) {
     mWindowManager = new WeakReference<>(vm);
+    mUseDisplayManager = false;
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+  public static void setCurrentDisplayManager(DisplayManager dm) {
+    if (dm != null) {
+      updateFreshRate();
+      mDisplayManager = dm;
+      if (mDisplayListener == null) {
+        mDisplayListener = new DisplayListener();
+      }
+      dm.registerDisplayListener(mDisplayListener, new Handler(Looper.getMainLooper()));
+      mUseDisplayManager = true;
+    }
   }
 
   public static void initUIThreadChoreographer() {
@@ -68,20 +131,27 @@ public class VSyncMonitor {
     }
   }
 
-  private static void doFrame(long nativePtr, long frameTimeNanos) {
-    long frameRefreshTimeNS = DEFAULT_FRAME_TIME_NS;
-    try {
-      WindowManager wm = mWindowManager.get();
-      if (wm != null) {
-        frameRefreshTimeNS = (long) (1000000000.0 / wm.getDefaultDisplay().getRefreshRate());
+  private static long getRefreshRate() {
+    if (!mUseDisplayManager) {
+      try {
+        mFrameRefreshTimeNS = DEFAULT_FRAME_TIME_NS;
+        WindowManager wm = mWindowManager.get();
+        if (wm != null) {
+          mFrameRefreshTimeNS = (long) (1000000000.0 / wm.getDefaultDisplay().getRefreshRate());
+        }
+      } catch (RuntimeException e) {
+        // These code contains an inter-process communication, which may throw DeadSystemException.
+        // And inside DisplayManagerGlobal, the DeadSystemException is wrapped into a
+        // RuntimeException and rethrown, so we attempt to catch RuntimeException here.
+        LLog.e("VSyncMonitor", "getRefreshRate failed: " + e.getMessage());
       }
-    } catch (RuntimeException e) {
-      // These code contains an inter-process communication, which may throw DeadSystemException.
-      // And inside DisplayManagerGlobal, the DeadSystemException is wrapped into a RuntimeException
-      // and rethrown, so we attempt to catch RuntimeException here.
-      LLog.e("VSyncMonitor", "getRefreshRate failed: " + e.getMessage());
     }
-    nativeOnVSync(nativePtr, frameTimeNanos, frameTimeNanos + frameRefreshTimeNS);
+
+    return mFrameRefreshTimeNS;
+  }
+
+  private static void doFrame(long nativePtr, long frameTimeNanos) {
+    nativeOnVSync(nativePtr, frameTimeNanos, frameTimeNanos + getRefreshRate());
   }
 
   private static native void nativeOnVSync(
